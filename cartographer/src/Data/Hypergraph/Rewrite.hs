08eafd6878a171f0160b11203f13513febd6ce0c
Paul Wilson <sf@statusfailed.com>
2019-04-04 09:13:07 +0100

    finish Data.Hypergraph.Rewrite.rewrite

--- cartographer/src/Data/Hypergraph/Rewrite.hs
+++ cartographer/src/Data/Hypergraph/Rewrite.hs
@@ -26,73 +26,87 @@ import qualified Data.Map.Strict as Map
 -- 4) Add in all the non-boundary ports of RHS, by replacing generator IDs with
 --    those in their match states
 rewrite
-  :: MatchState sig     -- ^ Matched pattern
+  :: Signature sig
+  => MatchState sig     -- ^ Matched pattern
   -> OpenHypergraph sig -- ^ Replacement for matched pattern (i.e. rule RHS)
   -> OpenHypergraph sig -- ^ Context of matching
   -> (OpenHypergraph sig, MatchState sig)
   -- ^ Rewritten graph and "match" subgraph identifying where RHS was added
-rewrite match rhs context = (c, rhsMatch3)
+rewrite lhsMatch rhs context = (c, rhsMatch2)
   where
-    a              = removeMatchEdges match context
+    a              = removeLhsEdges lhsMatch context
     (b, rhsMatch1) = addRhsEdges rhs context
-    rhsMatch2      = addBoundaries match rhsMatch1
-    (c, rhsMatch3) = addRhsConnections rhs rhsMatch2 b
+    rhsMatch2      = addBoundaries lhsMatch rhsMatch1
+    c              = addRhsConnections rhs rhsMatch2 b
 
 -- | Remove all edges in a 'MatchState' from an 'OpenHypergraph'.
-removeMatchEdges :: MatchState sig -> OpenHypergraph sig -> OpenHypergraph sig
-removeMatchEdges matchState context = foldr deleteEdge context edges
+removeLhsEdges :: MatchState sig -> OpenHypergraph sig -> OpenHypergraph sig
+removeLhsEdges matchState context = foldr deleteEdge context edges
   where
     -- list of the edges in the context which were part of the matching
     edges = fmap snd . Bimap.toList . _matchStateEdges $ matchState
 
 -------------------------------
--- Add RHS edges to the context
+-- addRhsEdges
+-- TODO: rethink time!
 
--- | Add all the edges of a pattern into a context, and return the new graph,
--- and the correspondence between pattern edges and (new) context edges.
---
--- statefully with new matching
--- for each (id, generator) in rhs:
---    id' <- addEdge generator context
---    add (id <-> id')
+-- 1) Copy all edges + non-boundary ports into the matching, and add to the
+-- context graph.
+-- 2) Copy all RHS boundary ports into the matching
+-- 3) Add connections to context graph
+
 addRhsEdges
-  :: OpenHypergraph sig
+  :: Signature sig
+  => OpenHypergraph sig
   -> OpenHypergraph sig
   -> (OpenHypergraph sig, MatchState sig)
 addRhsEdges rhs context =
-  let (context', pairs) = copyEdges rhs context
-  in  (context', emptyMatchState { _matchStateEdges = Bimap.fromList pairs })
-
--- | Copy all the edges from the source hypegraph to the target hypergraph,
--- returning also the correspondence between source hyperedge IDs, and their
--- new IDs in the target.
-copyEdges
-  :: Hypergraph f sig
-  -> Hypergraph f sig
-  -> (Hypergraph f sig, [(HyperEdgeId, HyperEdgeId)])
-copyEdges source target =
-  List.mapAccumL f target $ Map.toList (signatures source)
+  foldr f (context, emptyMatchState) (Map.toList $ signatures rhs)
   where
-    f graph (e, sig) = let (e', graph') = addEdge sig graph in (graph', (e, e'))
+    f (e, sig) (g, m) = copyEdge e sig g m
+
+copyEdge
+  :: Signature sig
+  => HyperEdgeId
+  -- ^ ID of edge in RHS pattern
+  -> sig
+  -- ^ signature of edge
+  -> OpenHypergraph sig
+  -- ^ Context graph
+  -> MatchState sig
+  -- ^ Current match state to modify
+  -> (OpenHypergraph sig, MatchState sig)
+  -- ^ Updated context graph and new match state
+copyEdge e sig graph match = (graph', match')
+  where
+    (e', graph') = addEdge sig graph
+    match' = match
+      { _matchStatePortsSource =
+          insertAll (_matchStatePortsSource match) sources
+      , _matchStatePortsTarget =
+          insertAll (_matchStatePortsTarget match) targets
+      , _matchStateEdges = Bimap.insert e e' (_matchStateEdges match)
+      }
+
+    (n, k) = toSize sig
+    sources = f e e' k
+    targets = f e e' n
+    f e e' m = [ (Port (Gen e) i, Port (Gen e') i) | i <- [0..m-1] ]
 
 -------------------------------
 -- addBoundaries
 
--- | Add the boundary ports from a LHS matching to the RHS matching
---
--- NOTE: WARNING: this actually wipes out existing ports in the replacement
--- match- this only works because its just a step in 'rewrite', but it would be
--- totally broken to use by itself!
+-- | Copy the boundary ports from a LHS matching to the RHS matching
 addBoundaries :: MatchState sig -> MatchState sig -> MatchState sig
 addBoundaries lhsMatch replacementMatch = replacementMatch
   { _matchStatePortsSource =
-      Bimap.filter (\x _ -> isBoundary x) . _matchStatePortsSource $ lhsMatch
+      insertAll (_matchStatePortsSource replacementMatch) sources
   , _matchStatePortsTarget =
-      Bimap.filter (\x _ -> isBoundary x) . _matchStatePortsTarget $ lhsMatch
+      insertAll (_matchStatePortsTarget replacementMatch) targets
   }
-
-isBoundary :: Port a Open -> Bool
-isBoundary (Port e _) = open True (const False) e
+  where
+    sources = boundaryPorts (_matchStatePortsSource lhsMatch)
+    targets = boundaryPorts (_matchStatePortsTarget lhsMatch)
 
 -------------------------------
 -- addRhsConnections
@@ -103,12 +117,37 @@ addRhsConnections
   :: OpenHypergraph sig
   -- ^ the rewrite rule\'s RHS
   -> MatchState sig
-  -- ^ the current match state of the RHS, containing matched edges and
-  -- boundary ports
+  -- ^ the current match state of the RHS, containing matched edges and ports
   -> OpenHypergraph sig
   -- ^ the current context graph, containing the (new) RHS edges
-  -> (OpenHypergraph sig, MatchState sig)
-addRhsConnections rhs rhsMatching context = undefined
+  -> OpenHypergraph sig
+addRhsConnections rhs rhsMatching context =
+  foldr (maybe id (uncurry connect)) context wires
   where
-    conns :: Bimap (Port Source Open) (Port Target Open)
-    conns = connections rhs
+    f = uncurry (translateWire rhsMatching)
+    wires = fmap f . Bimap.toList . connections $ rhs
+
+-- | Look up a source and target port in a matching and return the matched
+-- versions.
+translateWire
+  :: MatchState sig
+  -> Port Source Open
+  -> Port Target Open
+  -> Maybe (Port Source Open, Port Target Open)
+translateWire m s t = liftM2 (,) s' t'
+  where
+    s' = Bimap.lookup s (_matchStatePortsSource m)
+    t' = Bimap.lookup t (_matchStatePortsTarget m)
+
+-------------------------------
+-- Utilities
+
+insertAll :: (Ord a, Ord b) => Bimap a b -> [(a, b)] -> Bimap a b
+insertAll = foldr (uncurry Bimap.insert)
+
+boundaryPorts :: Bimap (Port a Open) (Port a Open) -> [(Port a Open, Port a Open)]
+boundaryPorts = Bimap.toList . Bimap.filter (\x _ -> isBoundary x)
+
+isBoundary :: Port a Open -> Bool
+isBoundary (Port e _) = open True (const False) e
+
