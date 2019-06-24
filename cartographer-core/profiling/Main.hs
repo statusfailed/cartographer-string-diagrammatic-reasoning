78c8c2c20b1d72d616640cb886e2f669e78f6439
Paul Wilson <sf@statusfailed.com>
2019-06-24 13:18:27 +0100

    Profiling function for matching

--- cartographer-core/profiling/Main.hs
+++ cartographer-core/profiling/Main.hs
@@ -5,17 +5,47 @@ import Data.List (foldl')
 import Data.Time.Clock
 
 data Generator = Generator (Int, Int)
+  deriving(Eq, Ord, Read, Show)
 
 instance Signature Generator where
   toSize (Generator x) = x
 
-main = do
+profileOp n (·) = do
   let a = singleton (Generator (1,2))
       b = singleton (Generator (2,1))
-      r = foldl' (→) empty (replicate 100000 $ a → b)
+      r = foldl' (·) empty (replicate n $ a → b)
   t0 <- getCurrentTime
   putStrLn ("started: " ++ show t0)
   print $ toSize r
   t1 <- getCurrentTime
   putStrLn ("finished: " ++ show t1)
   print (diffUTCTime t1 t0)
+
+-------------------------------
+
+composeProfile = profileOp 100000 (→)
+tensorProfile = profileOp 100000 (<>)
+
+-------------------------------
+
+matchProfile = do
+  let a = singleton (Generator (2,1))
+      b = singleton (Generator (2,2))
+      c = singleton (Generator (1,2))
+      r = foldl' (→) empty (replicate 2000 $ c → a)
+      g = r → b → r
+  t0 <- getCurrentTime
+  putStrLn ("mark: " ++ show t0)
+
+  print $ toSize g
+  t1 <- getCurrentTime
+  putStrLn ("mark: " ++ show t1)
+
+  print $ take 1 (match b g)
+  t2 <- getCurrentTime
+  putStrLn ("mark: " ++ show t2)
+
+  print (diffUTCTime t2 t1)
+
+main = matchProfile
+{-main = tensorProfile-}
